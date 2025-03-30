98eb513465e2ebbafde78ac7c529739b5a9c043c
Yehia Arja <yehia.al.arja12@gmail.com>
2025-03-31 02:25:07 +0300

    finished logging in and signing up

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SignupRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Services\ApiResponseService;
+use App\Models\User;
+use App\Http\Requests\LoginRequest;
+
+class AuthController extends Controller {
+    public function signup(SignupRequest $request) {
+        $credentials = $request->validated();
+
+        $user = new User($credentials);
+        $user->save();
+        return ApiResponseService::success('User created successfully', $user, 201);
+    }
+    public function login (LoginRequest $request) {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            return ApiResponseService::error('Invalid credentials', null, 401);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return ApiResponseService::success('Login successful', ['user' => $user,'token' => $token]);
+    }
+}
